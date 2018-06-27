9f41406d1adde8b84026b9c8f854146b5494f165
Lyniat <derlichking@gmx.net>
2018-06-27 10:50:09 +0200

    added m5stack configuration

---
+++ lpd-server-esp/com_2_7.py
@@ -0,0 +1,161 @@
+import sys
+import socket
+import time
+from PIL import Image
+import json
+
+
+'''
+    ESP_socket class handles the socket connection with ESP8266
+'''
+class ESP_Socket:
+
+    ANSWER = "OK"
+
+    def __init__(self, sock=None):
+        #                                 x           y           w           h           s
+        self.commandHeader = [0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x05, 0x00, 0x04, 0x00, 0x00, 0x02, 0x80, 0x19]
+
+        if sock is None:
+            self.sock = socket.socket()
+            self.sock.bind(("192.168.0.10",1516))
+        else:
+            self.sock = sock
+
+    def connect(self, host, port):
+        self.sock.connect((host, port))
+
+    def disconnect(self):
+        self.sock.close()
+
+    def send(self, bytes, isData = 0):
+        for b in bytes:
+            enc = bytearray()
+            enc.append(b)
+            try:
+                self.sock.send(enc)
+            except:
+                print "send failed"
+        print isData
+        if isData == 1:
+            answer = ""
+            while(True):
+                answer += self.sock.recv(1)
+                if answer == self.ANSWER:
+                    print "received: "+answer
+                    print "send next chunk!"
+                    break
+
+'''
+    handles communication with ESP8266 and creates sockets
+'''
+class Communicator:
+
+    def __init__(self,filename):
+
+        self.file = filename
+
+        self.CONF_FILE = './conf.json'
+
+        self.PORT = 0
+
+        self.BYTE_ZERO = 0x00
+
+        self.ip = ''
+
+        self.commandHeader = [0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x05, 0x00, 0x04, 0x00, 0x00, 0x02, 0x80, 0x10, 0x60, 0x00]
+
+        self.epdTemplate = ['\x3d', '\x04', '\x00', '\x05', '\x00', '\x01', '\x00', '\x00', '\x00', '\x00', '\x00', '\x00',
+                       '\x00', '\x00', '\x00', '\x00']
+
+        self.loadConf()
+
+        self.upload(filename)
+
+    def loadConf(self):
+        try:
+            conf = json.load(open(self.CONF_FILE))
+
+            self.ip = conf["client"]["ip"]
+            self.PORT = conf["client"]["port"]
+        except:
+            print "FAILED LOADING CONFIGURATION FILE"
+            sys.exit()
+
+    def chunks(self,l, n):
+        for i in xrange(0, len(l), n):
+            yield l[i:i + n]
+
+
+    def upload(self,filename):
+        data = []
+
+        starttime = time.time()
+
+        if filename[-4:].lower() == ".png" or filename[-4:].lower() == ".jpg": # might also work with other image formats
+            image = Image.open(filename)
+            image = image.convert('1') # convert to monochrome
+
+            # save pixel data into array. attention: it's still one byte per pixel!
+            image_data = [image.getpixel((x, y)) for y in range(image.height) for x in range(image.width)]
+
+            data.extend(self.epdTemplate) # workaround: epd header is not used but expected
+
+            # image data has to be one bit per pixel, so each byte of the source data
+            # has to be merged into one bit (which is enough for a monochrome image)
+            j = 8   # bit index of current byte
+            buf = 0 # buffer to store bits
+            for i in range(len(image_data)):
+                j -= 1
+
+                # shift new bit to left by index, then or it over the current buffer
+                buf |= (image_data[i] == 0) << j
+
+                # if our byte is completed, push it into data array
+                if j == 0:
+                    j = 8
+                    data.extend([chr(buf)])
+                    buf = 0
+
+        else:
+            print "Error: Unsupported file format!"
+            return
+
+        print("time for decoding image: "+str(time.time() - starttime))
+
+        print format(len(data), "#08x")
+
+        dataChunks = list(self.chunks(data, 24576)) #8192
+
+        print "number of chunks: "
+        print len(dataChunks)
+
+        print "send command header"
+        sock = ESP_Socket()
+        #sock.connect(self.ip, self.PORT)
+        sock.send(self.commandHeader)
+        sock.disconnect()
+
+        j = 0
+
+        print "start sending..."
+
+        print "send data"
+        for chunk in dataChunks:
+            print j
+            j += 1
+            sock = ESP_Socket()
+            sock.connect(self.ip, self.PORT)
+            sock.send(chunk, 1)
+            sock.disconnect()
+
+        print "finished"
+
+
+'''
+    entry point for this file
+    creates Communicator
+'''
+def upload(file):
+    Communicator(file)
+
---
+++ lpd-server-esp/server.py
@@ -0,0 +1,281 @@
+#!/usr/bin/env python
+
+import logging
+from zeroconf import ServiceInfo, Zeroconf, DNSOutgoing
+
+import socket
+import time
+import os
+import sys
+from PIL import Image, ImageFile
+from thread import start_new_thread
+import json
+import commands
+import get_ip
+
+import com_2_7
+
+class Server:
+
+    CONF_FILE = './conf.json'
+
+    TEMP_FILE = ''
+    CONVERTED_FILE = ''
+
+    WIDTH = 0
+    HEIGHT = 0
+
+    QUALITY = 1 #starting at 1
+
+    PRINTER_IP = ''
+    PRINTER_PORT = 0
+
+    waiting = False
+
+    startTime = -1
+
+    '''
+        load configuration file which can also be configured for your needs
+    '''
+    def loadConf(self):
+        try:
+            conf = json.load(open(self.CONF_FILE))
+
+            self.TEMP_FILE = conf["files"]["temp"]
+            self.CONVERTED_FILE = conf["files"]["converted"]
+
+            self.WIDTH = conf["image"]["width"]
+            self.HEIGHT = conf["image"]["height"]
+            self.QUALITY = conf["image"]["quality"]
+
+            self.PRINTER_IP = conf["printer"]["ip"]
+            self.PRINTER_PORT = conf["printer"]["port"]
+        except:
+            print "FAILED LOADING CONFIGURATION FILE"
+            sys.exit()
+
+    '''
+        the postscript driver writes "%%BoundingBox:" with values to the end of the file
+        but PIL can not convert the ps document if "%%BoundingBox:" has no values at the beginning
+        this fix writes the values from the last "%%BoundingBox:" to the first one
+    '''
+    def fixBoundingBox(self):
+        #read file
+        with open(self.TEMP_FILE, 'r') as file:
+            content = file.read()
+
+        #change bounding box
+        lines = content.split("\n")
+
+        beginning = 0
+        ending = len(lines) -1
+
+        while True:
+            line = lines[beginning]
+            if "%%BoundingBox:" in line:
+                break
+            else:
+                beginning += 1
+
+        while True:
+            line = lines[ending]
+            if "%%BoundingBox:" in line:
+                break
+            else:
+                ending -= 1
+
+        lines[beginning] = lines[ending]
+
+        changed = "\n".join(lines)
+
+        #delete old file
+        try:
+            os.remove(self.TEMP_FILE)
+        except OSError:
+            pass
+
+        #write to new file
+        f = open(self.TEMP_FILE, 'wb')
+        f.write(changed)
+        f.close()
+
+    '''
+        converts ps file to png and sends image to upload script
+    '''
+    def convert(self):
+        timeBeforeConversion = time.time()
+        print("fixing image")
+        self.fixBoundingBox() #PIL has problem with eps bounding box if not fixed
+        print("starting converting image")
+        image = Image.open(self.TEMP_FILE)
+        image.load(scale=self.QUALITY)
+        image = image.resize((self.HEIGHT,self.WIDTH), Image.ANTIALIAS)
+        image.save(self.CONVERTED_FILE)
+
+        try:
+            os.remove(self.TEMP_FILE)
+        except OSError:
+            pass
+
+        print("finished converting image")
+
+        deltaConversion = time.time() - timeBeforeConversion
+        print ("took " + str(deltaConversion) + " seconds for converting image")
+
+        timeBeforeUpload = time.time()
+        com_2_7.upload(self.CONVERTED_FILE)
+        deltaUpload = time.time() - timeBeforeUpload
+        print ("took " + str(deltaUpload) + " seconds for uploading and decoding image")
+
+        print ("UPLOADED SUCCESSFULLY!")
+        delta = time.time() - self.startTime
+        print ("took "+str(delta)+" seconds for whole process")
+        self.startTime = -1
+
+    '''
+        hosts the server which simulates an lpd printer
+    '''
+
+    def host(self):
+        sendingTime = -1
+        global resetTime
+        print("started server")
+        try:
+            os.remove(self.TEMP_FILE)
+        except OSError:
+            pass
+
+        ZERO = bytearray([0x00])
+
+        PACKET_SIZE = 1024
+
+        count = 0
+
+        mySocket = socket.socket()
+        mySocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        mySocket.bind((self.PRINTER_IP, self.PRINTER_PORT))
+
+        mySocket.listen(1)
+        conn, addr = mySocket.accept()
+        print ("Connection from: " + str(addr))
+        while True:
+            try:
+                data = conn.recv(PACKET_SIZE)
+
+                #print ("count: "+str(count))
+
+                if not data:
+                    time.sleep(0.001)
+                else:
+                    resetTime = time.time()
+                    if sendingTime < 0:
+                        sendingTime = time.time()
+                    #start measuring time
+                    if self.startTime < 0:
+                        self.startTime = time.time()
+                    #print ("from connected  user with length "+str(len(data)))
+                    if count >= 4:
+                        f = open(self.TEMP_FILE, 'ab')
+                        f.write(data.encode())
+                        f.close()
+
+                        if len(data) != PACKET_SIZE: #chance 1/PACKET_SIZE to fail
+                            conn.sendall(ZERO)
+                            conn.close()
+                    else:
+                        print (data)
+                        time.sleep(0.5)
+                        conn.sendall(ZERO)
+
+                    count += 1
+            except:
+                print ("no connection")
+                if count > 0:
+                    break
+
+
+        try:
+            deltaSending = time.time() - sendingTime
+            print("receiving data from CUPS took "+str(deltaSending))
+            self.convert()
+        except:
+            print("\n\nUPLOAD FAILED!\n")
+
+        try:
+            os.remove(self.CONVERTED_FILE)
+        except OSError:
+            pass
+
+    '''
+        starts and hosts the zeroconf service and restarts if it crashes
+    '''
+    def hostZeroConf(self):
+        while(True):
+            try:
+                self.zeroConf()
+            except:
+                print("Zeroconf failed. Starting again")
+            yield
+
+    '''
+        host zeroconf service, so that printer can be found in network
+        see: https://en.wikipedia.org/wiki/Zero-configuration_networking
+    '''
+    def zeroConf(self):
+        print("starting zeroconf")
+        #logging.basicConfig(level=logging.DEBUG)
+
+        desc = {}
+
+        info = ServiceInfo("_printer._tcp.local.",
+                           "e-Paper._printer._tcp.local.",
+                           socket.inet_aton(self.PRINTER_IP), self.PRINTER_PORT, 0, 0,
+                           desc, "lpd.local.")
+
+        zeroconf = Zeroconf()
+        zeroconf.register_service(info)
+        print("zeroconf registered device")
+
+
+resetTime = -1
+'''
+    program entry point
+    starts lpd socket
+    and zeroconf server in a new thread
+'''
+if __name__ == '__main__':
+    print("started application")
+
+    print("fetching esp ip")
+
+    '''
+        get ip from raspberry and save to config
+    '''
+    values = commands.getstatusoutput('sh get-ip.sh')
+    ip = values[1]
+    print (ip)
+    get_ip.change_ip(ip)
+
+    server = Server()
+
+    server.loadConf()
+
+    start_new_thread(server.zeroConf,())
+
+    global resetTime
+
+    while True:
+        try:
+            if resetTime > 0:
+                if time.time() - resetTime > 30: #max waiting time
+                    print("printing takes too long. probably something did not work as intended. script restarts now!")
+                    raise Exception('took too long!')
+            if server.waiting is False:
+                #try:
+                    server.host()
+               # except:
+                   # print("LPD Server failed. Starting again")
+        except Exception as err:
+            print("Exception: "+str(err))
+            print("starting server again")
+            resetTime = -1
